6e609ea968e3b4995a2f7e922bcfedc045190298
Geoffrey Tran <geoffrey.tran@gmail.com>
2013-11-12 16:22:23 -0600

    Fix for chef solo

--- recipes/default.rb
+++ recipes/default.rb
@@ -22,7 +22,7 @@
 include_recipe "mysql::ruby"
 
 execute "populate MySQL Time Zone Tables" do
-  command %{mysql_tzinfo_to_sql /usr/share/zoneinfo | "#{Chef::Config[:solo] ? 'mysql' : node['mysql']['mysql_bin']}" mysql -u root #{node['mysql']['server_root_password'].empty? ? '' : '-p=#{node['mysql']['server_root_password']}' }}
+  command %{mysql_tzinfo_to_sql /usr/share/zoneinfo | "#{Chef::Config[:solo] ? 'mysql' : node['mysql']['mysql_bin']}" mysql -u root #{node['mysql']['server_root_password'].empty? ? '' : "-p=#{node['mysql']['server_root_password']}" }}
 
   not_if do
     Gem.clear_paths
