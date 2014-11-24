a6ff575498fc8dac4273e55a6642b6d55fcc68e9
Geoffrey Tran <geoffrey.tran@gmail.com>
2014-11-23 22:57:07 -0600

    Update metadata.rb
    Adding name for berkshelf.

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,9 @@
+name             "mysql_timezone"
 maintainer       "Geoffrey Tran"
 maintainer_email "geoffrey.tran@gmail.com"
 license          "Apache License, Version 2.0"
 description      "Installs/Configures mysql's timezone"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.0.2"
 
 depends "mysql"
