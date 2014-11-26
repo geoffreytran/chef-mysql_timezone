f57eb480b1b6c64d5bedbfb35670bf6df7b8785c
Geoffrey Tran <geoffrey.tran@gmail.com>
2014-11-25 18:31:50 -0600

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,7 @@ maintainer_email "geoffrey.tran@gmail.com"
 license          "Apache License, Version 2.0"
 description      "Installs/Configures mysql's timezone"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.3"
+version          "0.0.4"
 
 depends "mysql"
+depends "mysql_chef_gem"
